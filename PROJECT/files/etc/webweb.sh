44a886f9ae5e80023c9d48c5081dd5ebfa880d08
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-19 17:10:42 +0800

    Update webweb.sh

--- PROJECT/files/etc/webweb.sh
+++ PROJECT/files/etc/webweb.sh
@@ -5,6 +5,6 @@ if [ -n "$(ls -A "/gzip/gzip_1.10-3_x86_64.ipk" 2>/dev/null)" ]; then
 fi
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
-sed -i '/webweb/d' /etc/crontabs/root
+sed -i '/webweb.sh/d' /etc/crontabs/root
 rm -rf /gzip
-rm -rf /etc/webweb
+rm -rf /etc/webweb.sh
