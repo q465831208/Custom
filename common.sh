fb83ba9cae1c157ccf44d26af3eb4a0ed6a6f068
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-19 21:29:40 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -297,10 +297,6 @@ case "${REPO_URL}" in
 	fi
 ;;
 esac
-if [[ "${TARGET_ADG}" == "x86-64" ]]; then
-	mkdir -p ${Home}/files/gzip
-	wget -P ${Home}/files/gzip https://downloads.openwrt.org/snapshots/packages/x86_64/packages/gzip_1.10-3_x86_64.ipk
-fi
 rm -rf {LICENSE,README,README.md,CONTRIBUTED.md,README_EN.md}
 rm -rf ./*/{LICENSE,README,README.md}
 rm -rf ./*/*/{LICENSE,README,README.md}
