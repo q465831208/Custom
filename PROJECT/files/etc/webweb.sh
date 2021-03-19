8217acc611e2567c72bf87c43c1e3cc935d5ca7a
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-19 21:30:06 +0800

    Update webweb.sh

--- PROJECT/files/etc/webweb.sh
+++ PROJECT/files/etc/webweb.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-if [ -n "$(ls -A "/gzip/gzip_1.10-3_x86_64.ipk" 2>/dev/null)" ]; then
-  opkg install /gzip/gzip_1.10-3_x86_64.ipk  --force-depends
-fi
 sed -i 's/<%=pcdata(ver.distversion)%>/<%=pcdata(ver.distversion)%><!--/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i 's/(<%=pcdata(ver.luciversion)%>)/(<%=pcdata(ver.luciversion)%>)-->/g' /usr/lib/lua/luci/view/admin_status/index.htm
 sed -i '/webweb.sh/d' /etc/crontabs/root
