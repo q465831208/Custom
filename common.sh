4d22e78aad6506db09891efcd591ce47bbef407a
q465831208 <75847963+q465831208@users.noreply.github.com>
2021-03-23 23:07:42 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -56,9 +56,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/danshui/luci-app
 git clone https://github.com/jerrykuku/luci-app-vssr package/danshui/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/danshui/luci-app-openclash
 git clone https://github.com/frainzy1477/luci-app-clash package/danshui/luci-app-clash
-#git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app-bypass
+git clone https://github.com/q465831208/bypass package/danshui/bypass
 
-git clone https://github.com/garypang13/openwrt-packages package/12
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 }
