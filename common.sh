30fdc12b00b97bc46815a761445f812b46bbee64
q465831208 <75847963+q465831208@users.noreply.github.com>
2021-06-19 00:01:30 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -58,7 +58,7 @@ git clone https://github.com/vernesong/OpenClash package/danshui/luci-app-opencl
 git clone https://github.com/frainzy1477/luci-app-clash package/danshui/luci-app-clash
 git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app-bypass
 git clone https://github.com/q465831208/bypass package/danshui/bypass
-#git clone https://github.com/q465831208/Redmi2100-WIFI-ProprietaryDriver-linux5.4 package/Redmi2100-WIFI
+git clone https://github.com/281677160/openwrt-package package/danshui/123
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 }
